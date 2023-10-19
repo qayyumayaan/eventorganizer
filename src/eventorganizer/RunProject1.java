750b161d410de4c1623c217d0aae5f721c571944
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-19 08:54:27 -0400

    Create all java files

--- src/eventorganizer/RunProject1.java
+++ src/eventorganizer/RunProject1.java
@@ -1,4 +1,10 @@
 package eventorganizer;
 
 public class RunProject1 {
+    /**
+     * Main driver for the EventOrganizer program.
+     * @param args
+     */
+    public static void main(String[] args) {
+        new EventOrganizer().run();}
 }
