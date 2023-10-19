c625dda8ed86347a66e9870bf13322f6c382cd15
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-19 09:19:16 -0400

    Write eventCal

--- src/eventorganizer/EventCalendar.java
+++ src/eventorganizer/EventCalendar.java
@@ -1,4 +1,28 @@
 package eventorganizer;
 
 public class EventCalendar {
+
+    private Event[] events;
+    private int numEvents;
+
+    private static final int INITIAL_SIZE = 4;
+    private static final int GROW_SIZE = 4;
+    private static final int NOT_FOUND = -1;
+
+    public EventCalendar() {
+        events = new Event[INITIAL_SIZE];
+        numEvents = 0;
+    }
+
+    public int getNumEvents() { return numEvents; }
+
+    private int find(Event event) {
+        for (int i = 0; i < numEvents; i++) {
+            if (events[i].equals(event))
+                return i;
+        }
+        return NOT_FOUND;
+    }
+
+
 }
