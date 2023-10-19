f3d1e9e25448db23e88b4d3f5b1884354da42b05
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-19 09:10:41 -0400

    Add javadoc for Timeslot

--- src/eventorganizer/Timeslot.java
+++ src/eventorganizer/Timeslot.java
@@ -1,6 +1,13 @@
 package eventorganizer;
-
+/**
+ * Timeslot class for representing different times of day for scheduling events.
+ *
+ * @author Ayaan Qayyum
+ */
 public class Timeslot {
+    /**
+     * Enumerated time slots.
+     */
 
     MORNING(10, 30),
     AFTERNOON(14,00),
@@ -13,21 +20,54 @@ public class Timeslot {
     private static final int MINUTES_PER_HOUR = 60;
 
 
+    /**
+     * Private constructor for Timeslot.
+     *
+     * @param hours The starting hour.
+     * @param minutes The starting minute.
+     * @author Ayaan Qayyum
+     */
     private Timeslot(int hours, int minutes) {
         this.hours = hours;
         this.minutes = minutes;
     }
 
+    /**
+     * Gets the hour component of the time slot.
+     *
+     * @return The starting hour.
+     * @author Ayaan Qayyum
+     */
     public int getHours() { return hours; }
 
+    /**
+     * Gets the minute component of the time slot.
+     *
+     * @return The starting minute.
+     * @author Ayaan Qayyum
+     */
     public int getMinutes() { return minutes; }
 
+
+    /**
+     * String representation of the Timeslot.
+     *
+     * @return A formatted string.
+     * @author Ayaan Qayyum
+     */
     public String toString() {
         if (hours < NOON) return String.format("%d:%02dam", hours, minutes);
         else if (hours == NOON) return String.format("%d:%02dpm", hours, minutes);
         else return String.format("%d:%02dpm", hours, 12, minutes);
     }
 
+    /**
+     * String representation of the Timeslot with duration.
+     *
+     * @param duration Duration in minutes to add.
+     * @return A formatted string.
+     * @author Ayaan Qayyum
+     */
     public String toString(int duration) {
         int time = hours * MINUTES_PER_HOUR + minutes + duration;
         int endHours = time / MINUTES_PER_HOUR;
