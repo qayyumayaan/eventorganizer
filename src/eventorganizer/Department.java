ea88e5c185ad687491ea21d1783de196b3f2b39a
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-19 09:14:52 -0400

    Write department enum

--- src/eventorganizer/Department.java
+++ src/eventorganizer/Department.java
@@ -1,4 +1,21 @@
 package eventorganizer;
 
-public class Department {
+public enum Department {
+    BAIT("Business Analytics and Information Technology"),
+    CS("Computer Science"),
+    EE("Electrical Engineering"),
+    ITI("Information Technology and Informatics"),
+    MATH("Mathematics");
+
+    private String name;
+
+    private Department(String name) {this.name = name;}
+
+    public String getName() { return name; }
+
+    @Override
+    public String toString() { return name; }
+
+
+
 }
