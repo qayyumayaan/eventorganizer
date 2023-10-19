e9c25f394d77b88f5eb1a19738d9778edbec8f0f
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-19 13:07:26 -0400

    Add basic vars

--- src/eventorganizer/EventOrganizer.java
+++ src/eventorganizer/EventOrganizer.java
@@ -1,4 +1,10 @@
 package eventorganizer;
 
 public class EventOrganizer {
+    EventCalendar eventCalendar = new EventCalendar();
+    Date date;
+    Timeslot startTime;
+    Location location;
+    Department department;
+    int duration;
 }
