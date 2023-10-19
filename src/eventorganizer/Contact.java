53ef4607a02ceeb55d615478411198924700d1b7
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-19 09:55:17 -0400

    Write contact enum

--- src/eventorganizer/Contact.java
+++ src/eventorganizer/Contact.java
@@ -1,4 +1,28 @@
 package eventorganizer;
 
 public class Contact {
+    private Department department;
+    private String email;
+
+    public Contact(Department department, String email) {
+        super();
+        this.department = department;
+        this.email = email;
+    }
+
+    public Department getDepartment() { return department; }
+
+    public String getEmail() { return email; }
+
+    public boolean isValid() {
+        if (department == null) return false;
+        if(email == null) return false;
+        if(!email.contains("@")) return false;
+        if (email.split("@").length != 2) return false;
+        if(!email.split("@")[1].equals("rutgers.edu")) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() { return department + ", " + email; }
 }
