de3bf693f5b4b13611c1501bed2d1a2c691b171c
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-19 10:14:39 -0400

    Write Location enum

--- src/eventorganizer/Location.java
+++ src/eventorganizer/Location.java
@@ -1,4 +1,28 @@
 package eventorganizer;
 
-public class Location {
+public enum Location {
+    HLL114("Hill Center", "Busch"),
+    ARC103("Allison Road Classroom", "Busch"),
+    TIL232("Tillett Hall", "Livingston"),
+    BE_AUD("Beck Hall", "Livingston"),
+    AB2225("Academic Building", "College Avenue"),
+    MU302("Murray Hall", "College Avenue");
+
+    private String buildingName;
+    private String campus;
+
+    private Location(String buildingName, String campus) {
+        this.buildingName = buildingName;
+        this.campus = campus;
+    }
+
+    public String getBuildingName() { return buildingName; }
+
+    public String getCampus() { return campus; }
+
+    @Override
+    public String toString() { return this.name() + " (" + buildingName + ", " + campus + ")";}
+
+
+
 }
