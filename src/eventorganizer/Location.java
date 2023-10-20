ddc840c927a7566a609887fcf954c1a290fbd228
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-20 08:59:45 -0400

    Change location enum var order

--- src/eventorganizer/Location.java
+++ src/eventorganizer/Location.java
@@ -1,17 +1,18 @@
 package eventorganizer;
 
 public enum Location {
-    HLL114("Hill Center", "Busch"),
+
     ARC103("Allison Road Classroom", "Busch"),
-    TIL232("Tillett Hall", "Livingston"),
-    BE_AUD("Beck Hall", "Livingston"),
+    HLL114("Hill Center", "Busch"),
     AB2225("Academic Building", "College Avenue"),
-    MU302("Murray Hall", "College Avenue");
+    MU302("Murray Hall", "College Avenue"),
+    BE_AUD("Beck Hall", "Livingston"),
+    TIL232("Tillett Hall", "Livingston");
 
     private String buildingName;
     private String campus;
 
-    private Location(String buildingName, String campus) {
+    Location(String buildingName, String campus) {
         this.buildingName = buildingName;
         this.campus = campus;
     }
