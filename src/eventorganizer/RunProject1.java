cbdc7c1e25c5c964ec1e0176efb624c675e27e3a
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-20 21:06:58 -0400

    Update RunProject1.java

--- src/eventorganizer/RunProject1.java
+++ src/eventorganizer/RunProject1.java
@@ -1,9 +1,15 @@
 package eventorganizer;
 
+/**
+ * Runs the Event program.
+ * @author Ayaan Qayyum
+ */
+
 public class RunProject1 {
     /**
      * Main driver for the EventOrganizer program.
      * @param args
+     * @author Ayaan Qayyum
      */
     public static void main(String[] args) {
         new EventOrganizer().run();}
