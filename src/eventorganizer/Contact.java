18ab5933656c510e8a546d063ea0e5c299ffdfff
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-20 21:03:56 -0400

    Update Contact javadoc

--- src/eventorganizer/Contact.java
+++ src/eventorganizer/Contact.java
@@ -1,19 +1,57 @@
 package eventorganizer;
 
+/**
+ * Represents a contact associated with an event, including department and email details.
+ *
+ * @author Ayaan Qayyum
+ */
 public class Contact {
+
+    /**
+     * The department associated with the contact.
+     */
     private Department department;
+
+    /**
+     * The email of the contact.
+     */
     private String email;
 
+    /**
+     * Constructor for Contact.
+     *
+     * @param department The department associated with the contact
+     * @param email      The email of the contact
+     */
     public Contact(Department department, String email) {
         super();
         this.department = department;
         this.email = email;
     }
 
-    public Department getDepartment() { return department; }
+    /**
+     * Gets the department associated with the contact.
+     *
+     * @return The department
+     */
+    public Department getDepartment() {
+        return department;
+    }
 
-    public String getEmail() { return email; }
+    /**
+     * Gets the email of the contact.
+     *
+     * @return The email
+     */
+    public String getEmail() {
+        return email;
+    }
 
+    /**
+     * Validates the email and department.
+     *
+     * @return true if valid, false otherwise
+     */
     public boolean isValid() {
         if (department == null) return false;
         if(email == null) return false;
@@ -23,6 +61,11 @@ public class Contact {
         return true;
     }
 
+    /**
+     * Returns the string representation of the contact.
+     *
+     * @return A string containing the department and email
+     */
     @Override
     public String toString() { return department + ", " + email; }
 }
