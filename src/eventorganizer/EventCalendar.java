c4510f727408f6cb4647132b91068b753bc22708
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-10-20 21:12:26 -0400

    Write javadoc for EventCal

--- src/eventorganizer/EventCalendar.java
+++ src/eventorganizer/EventCalendar.java
@@ -1,5 +1,11 @@
 package eventorganizer;
 
+/**
+ * Manages a dynamic array of Event objects.
+ *
+ * @author Ayaan Qayyum
+ */
+
 public class EventCalendar {
 
     private Event[] events;
@@ -9,13 +15,29 @@ public class EventCalendar {
     private static final int GROW_SIZE = 4;
     private static final int NOT_FOUND = -1;
 
+    /**
+     * Constructs an empty EventCalendar.
+     * @author Ayaan Qayyum
+     */
     public EventCalendar() {
         events = new Event[INITIAL_SIZE];
         numEvents = 0;
     }
 
+    /**
+     * Returns the current number of Events.
+     * @return The current number of Events.
+     * @author Ayaan Qayyum
+     */
     public int getNumEvents() { return numEvents; }
 
+
+    /**
+     * Finds the index of an Event.
+     * @param event The Event to find.
+     * @return The index or NOT_FOUND if not found.
+     * @author Ayaan Qayyum
+     */
     private int find(Event event) {
         for (int i = 0; i < numEvents; i++) {
             if (events[i].equals(event))
@@ -24,6 +46,10 @@ public class EventCalendar {
         return NOT_FOUND;
     }
 
+    /**
+     * Expands the internal array size.
+     * @author Ayaan Qayyum
+     */
     private void grow() {
         Event[] eventsNew = new Event[events.length + GROW_SIZE];
         for (int i = 0; i < events.length; i++) {
@@ -32,8 +58,22 @@ public class EventCalendar {
         events = eventsNew;
     }
 
+    /**
+     * Checks if the Event exists.
+     *
+     * @param event The Event to check.
+     * @return True if it exists, false otherwise.
+     * @author Ayaan Qayyum
+     */
     public boolean contains(Event event) { return find(event) != NOT_FOUND; }
 
+    /**
+     * Adds an Event.
+     *
+     * @param event The Event to add.
+     * @return True if added, false otherwise.
+     * @author Ayaan Qayyum
+     */
     public boolean add(Event event) {
         if (contains(event)) return false;
         if (numEvents == events.length) grow();
@@ -41,6 +81,14 @@ public class EventCalendar {
         return true;
     }
 
+
+    /**
+     * Removes an Event.
+     *
+     * @param event The Event to remove.
+     * @return True if removed, false otherwise.
+     * @author Ayaan Qayyum
+     */
     public boolean remove(Event event) {
         if (!contains(event)) return false;
         int index = find(event);
@@ -54,6 +102,10 @@ public class EventCalendar {
         for (int i = 0; i < numEvents; i++) System.out.println(events[i]);
     }
 
+    /**
+     * Prints Events sorted by date.
+     * @author Ayaan Qayyum
+     */
     public void printByDate() {
         for (int i = 0; i < numEvents; i++) {
             for (int j = 0; j < numEvents - 1; j++) {
@@ -67,12 +119,23 @@ public class EventCalendar {
         print();
     }
 
+    /**
+     * Prints Events sorted by campus.
+     * @author Ayaan Qayyum
+     */
     public void printByCampus() {
 
+        for (int i = 0; i < numEvents; i++) {
+            for (int j = 0; j < numEvents - 1; j++) {
+                if (events[j].getDate().compareTo(events[j + 1].getDate()) > 0) {
+                    Event temp = events[j];
+                    events[j] = events[j+1];
+                    events[j+1] = temp;
                 }
             }
         }
 
+        for (int i = 0; i < numEvents; i++) {
             for (int j = 0; j < numEvents - 1; j++) {
         for (int i = 0; i < numEvents; i++) {
             for (int j = 0; j < numEvents - 1; j++) {
@@ -83,15 +146,18 @@ public class EventCalendar {
                 }
             }
         }
+
+
+
         print();
     }
 
     public void printByDepartment() {
-        for (int i = 0; i < numEvents; i++) {
-            for (int j = 0; j < numEvents - 1; j++) {
-                if (events[j].getContact().getDepartment().compareTo(events[j + 1].getContact().getDepartment()) > 0){
+        for (int i = 0; i < numEvents - 1; i++) {
+            for (int j = 0; j < numEvents - i - 1; j++) {
+                if (events[j].getContact().getDepartment().getName().compareTo(events[j + 1].getContact().getDepartment().getName()) > 0) {
                     Event temp = events[j];
-                    events[j] = events[j+ 1];
+                    events[j] = events[j + 1];
                     events[j + 1] = temp;
                 }
             }
